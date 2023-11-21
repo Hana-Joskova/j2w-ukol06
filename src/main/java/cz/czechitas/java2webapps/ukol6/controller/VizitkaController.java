65cd57d9ec3ca20f9625066cbfa48ba64c4fc9fc
josko <joskovahanka@seznam.cz>
2023-11-21 20:45:58 +0100

    pridana funkcionalita pro pridavani vizitek a ulozeni do databaze

---
+++ src/main/java/cz/czechitas/java2webapps/ukol6/controller/VizitkaController.java
@@ -0,0 +1,66 @@
+package cz.czechitas.java2webapps.ukol6.controller;
+
+import cz.czechitas.java2webapps.ukol6.entity.Vizitka;
+import cz.czechitas.java2webapps.ukol6.repository.VizitkaRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class VizitkaController {
+
+    private final VizitkaRepository vizitkaRepository;
+
+    @Autowired
+    public VizitkaController(VizitkaRepository repository)
+    {
+        this.vizitkaRepository = repository;
+    }
+
+    @InitBinder
+    public void nullStringBinding(WebDataBinder binder) {
+        //prázdné textové řetězce nahradit null hodnotou
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    }
+
+    @GetMapping("/")
+    public ModelAndView seznam() {
+        return new ModelAndView("seznam")
+                .addObject("osoby", vizitkaRepository.findAll());
+    }
+
+    @GetMapping("/nova")
+    public ModelAndView novaVizitka() {
+        return new ModelAndView("formular")
+                .addObject("osoba", new Vizitka());
+    }
+
+    @PostMapping("/nova")
+    public String pridat(@ModelAttribute("osoba") @Valid Vizitka osoba, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "detail";
+        }
+        vizitkaRepository.save(osoba);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id:[0-9]+}")
+    public Object detail(@PathVariable Integer id) {
+        var vizitka = vizitkaRepository.findById(id);
+        if(vizitka.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        else {
+            return new ModelAndView("vizitka")
+                    .addObject("vizitka", vizitka.get());
+        }
+    }
+}
+
+
---
+++ src/main/java/cz/czechitas/java2webapps/ukol6/entity/Vizitka.java
@@ -0,0 +1,124 @@
+package cz.czechitas.java2webapps.ukol6.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+public class Vizitka {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Length(max = 100)
+    @NotEmpty
+    private String celeJmeno;
+
+    @Length(max = 100)
+    @NotEmpty
+    private String firma;
+
+    @Length(max = 100)
+    @NotEmpty
+    private String ulice;
+
+    @Length(max = 100)
+    @NotEmpty
+    private String obec;
+
+    @Length(max = 5)
+    @Length(min = 5)
+    @NotEmpty
+    private String psc;
+
+    @Length(max = 100)
+    private String email;
+
+    @Length(max = 20)
+    private String telefon;
+
+    @Length(max = 100)
+    private String web;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCeleJmeno() {
+        return celeJmeno;
+    }
+
+    public void setCeleJmeno(String celeJmeno) {
+        this.celeJmeno = celeJmeno;
+    }
+
+    public String getFirma() {
+        return firma;
+    }
+
+    public void setFirma(String firma) {
+        this.firma = firma;
+    }
+
+    public String getUlice() {
+        return ulice;
+    }
+
+    public void setUlice(String ulice) {
+        this.ulice = ulice;
+    }
+
+    public String getObec() {
+        return obec;
+    }
+
+    public void setObec(String obec) {
+        this.obec = obec;
+    }
+
+    public String getPsc() {
+        return psc;
+    }
+
+    public void setPsc(String psc) {
+        this.psc = psc;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+
+    public String getCelaAdresa() {
+        return ulice + ", " + obec + ", " + psc;
+    }
+
+}
